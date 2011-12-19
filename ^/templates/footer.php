08ca1aa7b49910a777bc2e2ad5baa9a2abb7888b
LoneFry <dev@lonefry.com>
2011-12-19 15:12:16 -0500

    Add test for G:: in cases of DB-less operation; beautify

--- ^/templates/footer.php
+++ ^/templates/footer.php
@@ -1,6 +1,6 @@
 		</div>
 		<div id="footer">
-<?php if(G::$S->Login){ ?>
+<?php if (G::$S && G::$S->Login) { ?>
 			<p>
 				<a href="<?php echo CONT; ?>Account/edit">Account Settings</a>
 			</p>
@@ -9,27 +9,33 @@
 				src="<?php echo CORE; ?>/images/valid-html401" 
 				alt="Valid HTML 4.01" class="webButton"></a>
 			<br>
-			<?php if(isset(G::$G['startTime']))echo '<span class="subtle">load time: '.number_format(microtime(true)-G::$G['startTime'],4).'s</span>'; ?>
+<?php 
+if (isset(G::$G['startTime'])) {
+	echo '<span class="subtle">load time: '.number_format(microtime(true)-G::$G['startTime'], 4).'s</span>';
+}
+?>
 		</div>
 		
 		<script type="text/javascript"><!--// add icon to external links
 			var s=location.protocol+'//'+location.hostname;
 			var a=document.getElementsByTagName('a');
-			for(f in a){
-				if(a[f].href && 
-					a[f].href.substr(0,4)=='http' && 
-					a[f].href.substr(0,s.length)!=s &&
-					(' '+a[f].rel+' ').indexOf(' external ')==-1 &&
-					a[f].getElementsByTagName('img').length==0
-				){
-					a[f].rel+=' external';
+			for (f in a) {
+				if (a[f].href
+					&& a[f].href.substr(0, 4) == 'http' 
+					&& a[f].href.substr(0, s.length) != s
+					&& (' '+a[f].rel+' ').indexOf(' external ') == -1
+					&& a[f].getElementsByTagName('img').length == 0
+				) {
+					a[f].rel += ' external';
 				}
 			}
 		//--></script>
 <?php
-if(MODE=='dev'){
-	if(isset($_POST))G::croak($_POST,false);
-	G::croak(G::$M->getQueries(),false);
+if (MODE == 'dev') {
+	if (isset($_POST)) {
+		G::croak($_POST, false);
+	}
+	G::croak(G::$M->getQueries(), false);
 }
 ?>
 	</body>
