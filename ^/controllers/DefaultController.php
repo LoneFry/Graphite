deb2a524ab88f9413102c4576d9856e1a402644d
LoneFry <dev@lonefry.com>
2013-03-09 22:38:18 -0500

    Adjust Dispatcher's routing
    Remove vestigial Dispatcher->action/()
    Move 404/500 actions from Controller base class to DefaultController to better
     distinguish default error handling from custom error handling
    Adjust Dispatcher->Act() to handle 404 routing

--- ^/controllers/DefaultController.php
+++ ^/controllers/DefaultController.php
@@ -13,4 +13,32 @@
  ****************************************************************************/
 
 class DefaultController extends Controller {
+	protected $action = '404';
+
+	/**
+	 * default action for handling 404 errors
+	 *
+	 * @param array $argv request parameters
+	 *
+	 * @return void
+	 */
+	public function do_404($argv) {
+		header("HTTP/1.0 404 File Not Found");
+		G::$V->_template = '404.php';
+		G::$V->_title    = 'Requested Page Not Found';
+	}
+
+	/**
+	 * default action for handling 500 errors
+	 *
+	 * @param array $argv request parameters
+	 *
+	 * @return void
+	 */
+	public function do_500($argv) {
+		header("HTTP/1.0 500 Internal Server Error");
+		G::$V->_template = '500.php';
+		G::$V->_title    = 'Internal Server Error';
+	}
 }
+
