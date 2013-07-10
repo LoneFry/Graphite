aae337376705b732e562b26130b711afc7b06dba
LoneFry <dev@lonefry.com>
2013-07-10 17:27:46 -0400

    Adjust how GET/POST values are passed to action methods

--- ^/lib/Controller.php
+++ ^/lib/Controller.php
@@ -89,7 +89,27 @@ abstract class Controller {
 			$argv = $this->argv;
 		}
 		$func = 'do_'.$this->action;
-		$this->$func($argv);
+
+		// non-numeric $_GET keys override $argv keys
+		foreach ($_GET as $key => $val) {
+			if (!is_numeric($key)) {
+				$argv[$key] = $val;
+			}
+		}
+
+		switch ($_SERVER['REQUEST_METHOD']) {
+			case 'GET':
+				$params = $_GET;
+				break;
+			case 'POST':
+				$params = $_POST;
+				break;
+			default:
+				$params = array();
+				break;
+		}
+
+		$this->$func($argv, $params);
 	}
 
 	/**
